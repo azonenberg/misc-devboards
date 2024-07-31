f6ca51c5891d61f5dbbce998923b534aaea7eae5
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2024-07-31 00:59:31 -0700

    imagesize: report ITCM usage

--- fpga-stm32-ifaces/firmware/main/imagesize.sh
+++ fpga-stm32-ifaces/firmware/main/imagesize.sh
@@ -2,10 +2,14 @@
 LINE=`ls -lh firmware.bin | cut -d ' ' -f 5`
 echo "Flash memory usage:     $LINE";
 
+ITCMSTART=$(arm-none-eabi-objdump -t firmware.elf  | grep __itcm_start | cut -d ' ' -f 1);
+ITCMEND=$(arm-none-eabi-objdump -t firmware.elf  | grep __itcm_end | cut -d ' ' -f 1);
 TCMSTART=$(arm-none-eabi-objdump -t firmware.elf  | grep __tcmstart | cut -d ' ' -f 1);
 STACKSTART=$(arm-none-eabi-objdump -t firmware.elf  | grep __end | cut -d ' ' -f 1);
 STACKEND=$(arm-none-eabi-objdump -t firmware.elf  | grep __stack | cut -d ' ' -f 1);
 DTCMSTART=$(echo "obase=10;ibase=16;${TCMSTART^^}" | bc);
+XITCMSTART=$(echo "obase=10;ibase=16;${ITCMSTART^^}" | bc);
+XITCMEND=$(echo "obase=10;ibase=16;${ITCMEND^^}" | bc);
 DSTACKSTART=$(echo "obase=10;ibase=16;${STACKSTART^^}" | bc);
 DSTACKEND=$(echo "obase=10;ibase=16;${STACKEND^^}" | bc);
 STACKSIZE=$(expr $DSTACKEND - $DSTACKSTART);
@@ -34,3 +38,6 @@ echo "Global TCM BSS size:    $TCMSIZE bytes";
 GLOBALSIZE=$(expr $DATASIZE + $BSSSIZE + $TCMSIZE);
 GLOBALKB=$(expr $GLOBALSIZE / 1024);
 echo "Total global size:      $GLOBALSIZE bytes";
+
+ITCMSIZE=$(expr $XITCMEND - $XITCMSTART);
+echo "ITCM usage:             $ITCMSIZE bytes";
